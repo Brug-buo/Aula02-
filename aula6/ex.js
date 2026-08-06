abe4f44b8c61eebc7c1b137dd8469fad7062520e
etebillu <pedro.prestes.oliveira@escola.br.gov.br>
2026-08-06 11:15:46 -0300

    aula06 - CRUD completo em memoria

--- aula6/ex.js
+++ aula6/ex.js
@@ -1,6 +1,6 @@
-const express = require ('express') ;
-const app = express () ;
-app.use ( express.json () ) ;
+const express = require ('express');
+const app = express ();
+app.use ( express.json () );
 
 let prodcor = [
   {id: 1, nome: "verde", preco: 1.00}, 
@@ -11,10 +11,12 @@ let proxid = 4;
 
 
 
+
 // GET /-> retorna todos os produtos
 app.get ('/', (req, res) => {
 res.json(prodcor);
 });
+
 
 
 
@@ -30,6 +32,7 @@ res.json (produto) ;
 
 
 
+
 // POST /criar -> cria um novo produto
 app.post ('/criar', ( req , res ) => {
 console.log ( req.body.nome );
@@ -38,11 +41,46 @@ const novoProduto = {
      nome: req.body.nome,
      preco: req.body.preco
  };
-
+ 
  prodcor.push(novoProduto );
  proxid++;
  res.status (201).json (novoProduto);
 }) ;
+
+
+
+
+
+// PUT /atualiza/:id -> atualiza um produto existente
+app.put ('/atualiza/:id', (req, res) => {
+  console.log("ola");
+  const id = Number (req.params.id);
+  const produto = prodcor.find (a => a.id === id);
+  if (!produto){
+    return res.status(404).json ({ erro: "produto nao encontrado "});  
+  }
+  produto.nome = req.body.nome;
+  produto.preco = req.body.preco;
+  res.json(produto);
+});
+
+
+
+
+
+//DELETE /retira/:id -> remove produto
+app.delete ('/retira/:id', (req, res) => {
+const id = Number (req.params.id);
+const cor = prodcor.find( a => a.id === id);
+if(!cor){
+return res.status(404).json ({erro: "cor nao encontrada"})
+}
+prodcor = prodcor.filter(a => a.id !== id);
+res.json({mensagem: "Cor removida com sucesso"});  
+});
+
+
+
 
 
 app.listen(3000, () => {
